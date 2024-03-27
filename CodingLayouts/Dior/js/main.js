aaf12fa2cab62d6768eb0da4229207bd51735290
Volodymyr Bakumenko <volodya.bakumenko@gmail.com>
2024-03-27 14:46:15 +0200

    Додано секцію accordeon та дописані стилі і функціонал

--- CodingLayouts/Dior/js/main.js
+++ CodingLayouts/Dior/js/main.js
@@ -20,3 +20,27 @@ const swiperAbout = new Swiper(".about__slider", {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// const accordeonItems = document.querySelectorAll('.accordeon__item');
+// const accordeonTrigers = document.querySelectorAll('.accordeon__triger');
+
+// accordeonTrigers.forEach((accordeonTriger) => {
+//   console.log('accordeonTriger', accordeonTriger);
+//   accordeonTriger.addEventListener('click', () => {
+//     console.log('click');
+//     const accordeonItem = accordeonTriger.closest('.accordeon__triger').parentElement;
+//     console.log('accordeonItem', accordeonItem);
+//     accordeonItems.forEach((accordeonItem) => {
+//       if (accordeonItem !== accordeonItem) {
+//         accordeonItem.classList.remove('accordeon__item--active');
+//       }
+//     });
+//     accordeonItem.classList.toggle('accordeon__item--active');
+//   });
+// });
+
+document.querySelectorAll('.accordeon__triger').forEach((item) => {
+  item.addEventListener('click', () => {
+    item.parentNode.classList.toggle('accordeon__item--active');
+  });
+});
